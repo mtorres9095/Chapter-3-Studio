1e412c76e95ab7b171287ae368e42cadb2876238
mtorres9095 <mtorres9095@gmail.com>
2020-06-03 20:44:19 -0400

    Add project files.

---
+++ Chapter 3 Studio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter_3_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a text: ");
+            string text = Console.ReadLine().ToLower();
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (char character in text)
+            {
+                if (Char.IsLetter(character))
+                {
+                    if (charCount.ContainsKey(character)) 
+                    {
+                        charCount[character] += 1;
+                    }
+                    else      
+                    {
+                        charCount.Add(character, 1);
+
+                    }
+                }
+            }
+            foreach (KeyValuePair<char, int> count in charCount)
+            {
+             Console.WriteLine($"{count.Key} : {count.Value}");
+            }
+        }
+    }
+}
